eb186471423a6524e36d8385cb3b8abe230a17ca
mohamed.mansour@epitech.eu <mohamed.mansour@epitech.eu>
2023-02-24 09:24:27 +0000

    memset

---
+++ memset.asm
@@ -0,0 +1,17 @@
+BITS 64
+
+global memset
+    .memset:
+        mov rax,        rdi
+        mov rcx,        rdx
+        jmp             .loop
+
+    .loop:
+        cmp rcx,        0
+        je              .return
+        dec             rcx
+        mov [rax],   sil
+        jmp             .loop
+
+    .return:
+        ret
