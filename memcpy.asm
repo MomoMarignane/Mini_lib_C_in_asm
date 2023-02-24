9555df1b3f9dbcc8b9677ed18659cf47d3c42cd5
mohamed.mansour@epitech.eu <mohamed.mansour@epitech.eu>
2023-02-24 10:58:27 +0000

    memcpy

---
+++ memcpy.asm
@@ -0,0 +1,17 @@
+BITS 64
+
+global memcpy
+    .memcpy:
+        mov rax,        rdi
+        jmp             .loop
+
+    .loop:
+        mov rax,      rsi
+        inc             rsi
+        cmp rsi,        0
+        je              .return
+        jmp             .loop
+
+    .return:
+        ret
+
