75d5c5ea422cc29ad1f7423dccc1662c6469cb55
mohamed.mansour@epitech.eu <mohamed.mansour@epitech.eu>
2023-02-24 11:14:38 +0000

    strcmp

---
+++ strcmp.asm
@@ -0,0 +1,18 @@
+BITS 64
+
+global strcmp
+    .strcmp:
+        cmp rsi,    0
+        je          .true
+        cmp rsi,    rdi
+        je          .strcmp
+        jmp         .false
+
+    .true:
+        sub rsi,    rdi
+        mov rax,    rsi
+        ret
+
+    .false:
+        xor rax, rax
+        ret
